e14c10cc96e04ce0930326a11ca221c1e45723fa
Kenneth Wu <kennywu33@gmail.com>
2024-07-01 15:34:29 -0400

    connected to mongo and have first route

--- server/server.js
+++ server/server.js
@@ -1,18 +1,24 @@
-const express = require('express');
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import express from 'express'
 
 const app = express();
 const PORT = 3000;
-
-const planets = [
-    { id: 1, name: 'Mercury' },
-    { id: 2, name: 'Venus' },
-    { id: 3, name: 'Earth' },
-    // Add more planets as needed
-  ];
-
-app.get('/api/planets', (req, res) => {
-    res.json(planets);
-  });
+app.use(cors());
+dotenv.config();
+const url = process.env.MONGO_DB_URL;
+const dbName = process.env.MONGO_DB;
+const client = await MongoClient.connect(url);
+const db = client.db(dbName);
+app.get('/api/characters', async (req, res) => { 
+    try { 
+      const characters = await db.collection('characters').find().toArray(); 
+      res.json(characters); 
+    } 
+    catch (err) { 
+      res.status(500).json({ message: err.message }); 
+    } });
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);

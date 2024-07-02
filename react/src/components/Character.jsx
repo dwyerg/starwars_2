aa77c309004dd05302d70fad9af5dd6cb8057af2
Grace <ggdwyer@travelers.com>
2024-07-02 14:46:42 -0400

    added planet data for characters

--- react/src/components/Character.jsx
+++ react/src/components/Character.jsx
@@ -11,14 +11,17 @@ const Character = (props) => {
     const fetchData = async () => {
       try {
         const fresponse = await fetch(`${import.meta.env.VITE_CHARACTERS_API_URL}/${params.id}/films`);
+        const presponse = await fetch(`${import.meta.env.VITE_PLANETS_API_URL}/${params.id}`);
         const response = await fetch(`${import.meta.env.VITE_CHARACTERS_API_URL}/${params.id}`);
         if (!response.ok) {
           throw new Error('Data could not be fetched!');
         }
         const json_response = await response.json();
         const fjson_response = await fresponse.json();
+        const pjson_response = await presponse.json();
         setData(json_response);
         setFilmData(fjson_response);
+        setPlanetData(pjson_response);
       } catch (error) {
         console.error('Error fetching characters:', error);
       }
@@ -41,6 +44,7 @@ const Character = (props) => {
                 {filmdata.map((film) => (
                     <div key={film.id} div className="card-text">{film.title}</div>
                 ))}
+                <div className="card-text">Planet: {planetdata.name}</div>
             </div>
         </div>
     );
